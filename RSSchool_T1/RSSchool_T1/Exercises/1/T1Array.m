dc0910f933ab1e30237cf0e2b157b7c55a8ea83f
Alena Kazimerchyk <kasimertschikh@gmail.com>
2020-03-17 22:44:00 +0300

    feat: add Exercises1 Array

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,28 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if ([sadArray  isEqual: @[]]) {
+        
+        NSLog (@"Sad array is empty!");
+        return @[];
+    }
+    
+    NSMutableArray *resultArray = [NSMutableArray arrayWithObject: [sadArray firstObject]];
+    
+    for (int i = 1; i < sadArray.count - 1; i++) {
+        
+        if ([sadArray[i] intValue] <= [sadArray[i - 1] intValue] + [sadArray[i + 1] intValue])
+        {
+            [resultArray addObject:sadArray[i]];
+        }
+    }
+    [resultArray addObject: [sadArray lastObject]];
+    
+    if ([sadArray count] != [resultArray count]) {
+        resultArray = [NSMutableArray arrayWithArray:[self convertToHappy: resultArray]];
+    }
+    
+    return resultArray;
 }
 
 @end
