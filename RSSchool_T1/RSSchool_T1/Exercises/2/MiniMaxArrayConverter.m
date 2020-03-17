fe6466b761a6253e43f07c8bd102d6232f17f367
Alena Kazimerchyk <kasimertschikh@gmail.com>
2020-03-17 23:22:44 +0300

    feat: add Exercises2 MinMaxArray

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,30 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
-}
+    
+    NSMutableArray *workArray = [NSMutableArray arrayWithArray: array];
+  
+    NSNumber *maxNumber = workArray[0];
+    NSNumber *minNumber = workArray[0];
+    
+    NSInteger total = [workArray[0] intValue];
+        
+    for (int i = 1; i < [workArray count]; i++) {
+        
+        total += [workArray[i] intValue];
 
+        if ([minNumber intValue] > [workArray[i] intValue]) {
+            minNumber = workArray[i];
+        }
+        if ([maxNumber intValue] < [workArray[i] intValue]) {
+            maxNumber = workArray[i];
+        }
+    }
+    
+    NSInteger minTotalAmount = total - [maxNumber intValue];
+    NSInteger maxTotalAmount = total - [minNumber intValue];
+    return @[@(minTotalAmount), @(maxTotalAmount)];
+    }
+    
+    
 @end
