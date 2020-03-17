3fa07d8a75522265dd6f0ad6231817c6a4db441a
Alena Kazimerchyk <kasimertschikh@gmail.com>
2020-03-17 23:55:19 +0300

    feat: add Exercises3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -2,9 +2,26 @@
 
 @implementation BillCounter
 
-// Complete the following fuction
+// Complete the following function
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    NSMutableArray *totalBill = [NSMutableArray arrayWithArray:bill];
+    
+    [totalBill removeObjectAtIndex:index];
+    
+    int annaMoney = 0;
+    
+    for (int i = 0; i < [totalBill count]; i++) {
+        annaMoney += [totalBill [i] intValue];
+    }
+    
+    annaMoney /= 2;
+    
+    if (annaMoney == [sum intValue]) {
+        return @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat:@"%d", [sum intValue] - annaMoney ];
+    }
 }
 
 @end
